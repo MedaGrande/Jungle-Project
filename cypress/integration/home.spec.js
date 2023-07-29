80f8388fad271432c77f144b27a7f8dcc7055052
Medhanie Alene <alenemedhanie@gmail.com>
2023-07-29 18:45:27 -0400

    implement homepage test with cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Jungle app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('visits homepage', () => {
+    
+  })
+
+  it("There is products on the page", () => {
+  cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+  cy.get(".products article").should("have.length", 12);
+});
+})
